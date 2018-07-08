db85b79807baef49b55715f8e481d412c911d158
Shashank Racherla <shashank.racherla@gmail.com>
2018-07-08 16:17:05 -0700

    added merge sort

--- Sorts.rb
+++ Sorts.rb
@@ -25,10 +25,10 @@ def optimized_bubble_sort(arr)
     prc ||= Proc.new { |x,y| x <=> y }
     lastSwap = arr.length-1;
     i = 0
-    while i<arr.length
+    while i < arr.length
         is_sorted = true;
         currentSwap = -1;
-        j = 0 
+        j = 0
         while j < lastSwap
             if prc.call(arr[j],arr[j+1]) > 0 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
@@ -41,8 +41,38 @@ def optimized_bubble_sort(arr)
         lastSwap = currentSwap;
         i+=1
     end
-
+    arr
 end
 
-res = optimized_bubble_sort([4,1,2,6,3,5,5,7,8,8,8,8,5])
-p res
+# res = optimized_bubble_sort([4,1,2,6,3,5,5,7,8,8,8,8,5])
+# p res
+
+def merge(left, right)
+    prc ||= Proc.new {|x,y| x <=> y}
+    result = []
+    until left.empty? || right.empty?
+        if prc.call(left[0],right[0]) == -1
+            result << left.shift
+        elsif prc.call(left[0], right[0]) == 1
+            result << right.shift
+        else 
+            result << left.shift
+        end
+    end
+    result + left + right
+end
+
+def merge_sort(arr)
+    return arr if arr.length <= 1
+
+    mid = arr.length/2
+    left = arr[0...mid]
+    right = arr[mid..-1]
+
+    merge(merge_sort(left), merge_sort(right))
+end
+
+
+
+# p merge_sort([2,1,3,57,57,3,2])
+
