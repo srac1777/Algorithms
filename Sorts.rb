b8598cc8e23441c61d6bad0b486f789b61ecccce
Shashank Racherla <shashank.racherla@gmail.com>
2018-07-09 14:15:43 -0700

    "no recursion quick sort in place"

--- Sorts.rb
+++ Sorts.rb
@@ -1,4 +1,4 @@
-
+require 'byebug'
 def bubble_sort(arr, &prc)
     prc ||= Proc.new { |x,y| x <=> y }
     return arr if arr.length <= 1
@@ -89,4 +89,40 @@ end
 
 # p og_quick_sort([2,1,3,57,57,3,2,0,-1])
 
+#  [ 4 , 1 , 3 , 5 , 6 , 3 , 2 , 0 , -1 ]
+#      [-1, 1, 3, 3, 2, 0, 4, 5, 6]
+#                           * =
 
+def in_place_quick_sort(arr, s_idx = 0, e_idx = arr.length)
+    # prc ||= Proc.new { |x,y| x <=> y }
+    return arr if arr.length <= 1
+
+    pivot = arr[s_idx...e_idx].first
+    boundary_idx = s_idx + 1
+
+    i = s_idx + 1
+    while i < e_idx
+        # debugger
+        if arr[i] >= pivot
+            i += 1
+        else
+            arr[boundary_idx], arr[i] = arr[i], arr[boundary_idx]
+            boundary_idx += 1
+
+            i += 1
+        end
+    end
+    arr[boundary_idx - 1], arr[0] = arr[0], arr[boundary_idx - 1]
+
+    # leftlen = boundary_idx - s_idx
+    # rightlen = arr[s_idx...e_idx].length - (boundary_idx + 1)
+
+    # in_place_quick_sort( arr, s_idx, boundary_idx - 1 )
+
+    # in_place_quick_sort( arr, boundary_idx + 1, e_idx )
+    arr
+
+    
+end
+
+ p in_place_quick_sort([4 , 1 , 3 , 2 , 5 , 6 , 3 , 7 , 0 , -1])
